901503fbf433429073a4077dc095854c0ea4f5fd
code <1165444511@qq.com>
2017-07-19 21:25:58 +0800

    connect local mysql database

--- server.js
+++ server.js
@@ -7,6 +7,24 @@ const app = new express();
 
 app.use(express.static(__dirname + '/public'));
 
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "user",
+    port:3306
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('connecting success');
+});
+
 app.listen('3000',()=>{
     console.log('3000 port is sucessful');
 });
